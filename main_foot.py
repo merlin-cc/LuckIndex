7f1956d71b351c712c6b7e18ecfc9bb0aed737fb
Antoinech18 <antoinech18@hotmail.fr>
2026-01-12 15:21:13 +0100

    debug 1 (à débuger encore)

--- main_foot.py
+++ main_foot.py
@@ -2,11 +2,12 @@ from Team import *
 from tirage_foot import *
 from draw_2026_WC import *
 
+data = pd.read_csv('football_ranking.csv')
+teams_list = teams_list(data, 1, len(data))
 
-
-distributions = run_simulation(teams_list, 100)
+distributions = run_simulation(teams_list, 1)
 real_draw = single_draw(pots)
 luckIndex = luck_index_foot(teams_list, distributions, real_draw)
 
-display_random_foot(distributions, luckIndex, teams_list, 3)
+display_random_foot(distributions, luckIndex, teams_list, 3, 10)
 plt.show()
